856ef2a28053f05a3242384e9c995ac1bcc38636
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2023-09-25 16:23:51 +0700

    Update StoryComponent.vue

--- src/components/StoryComponent.vue
+++ src/components/StoryComponent.vue
@@ -17,7 +17,79 @@
 			<!-- Timeline -->
 			<div class="w-auto px-4 mx-auto">
 				<ol class="w-auto max-w-md mx-auto xl:max-w-2xl">
-					<li class="flex flex-row mt-6">
+					<!-- Item list timeline -->
+					<li v-for="(tl, index) in histories" class="flex flex-row mt-6" :key="index">
+						<div class="hidden h-1 w-[480px] xl:block"></div>
+						<div class="w-6 h-[284px] xl:h-[304px]">
+							<div
+								class="flex items-center justify-center w-6 h-6 rounded-full"
+								:class="{
+									'bg-bgSecondary': index != 0,
+									'bg-primary': index == 0,
+								}"
+							>
+								<svg
+									aria-hidden="true"
+									focusable="false"
+									data-prefix="fas"
+									class="w-3 h-3"
+									:class="{
+										'text-bgSecondary': index == 0,
+										'text-primary': index != 0,
+									}"
+									role="img"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 448 512"
+								>
+									<path
+										fill="currentColor"
+										d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"
+									></path>
+								</svg>
+							</div>
+							<div
+								class="w-[3px] h-full mx-auto"
+								:class="{
+									'bg-bgSecondary': index != 0,
+									'bg-primary': index == 0,
+								}"
+							></div>
+						</div>
+						<div class="max-w-md xl:max-w-2xl">
+							<div class="w-auto">
+								<div class="block max-w-md p-6 ml-6 rounded-lg shadow-lg bg-bgSecondary">
+									<div class="flex justify-between mb-4">
+										<a href="#!" class="px-2 text-base font-medium -translate-x-2 rounded-full lan-text-primary">{{
+											tl.title
+										}}</a>
+										<p class="px-2 text-sm font-medium translate-x-2 text-primary">
+											{{ tl.date }}
+										</p>
+									</div>
+									<p class="mb-6 text-sm text-secondary">
+										{{ tl.desc }}
+									</p>
+									<button
+										type="button"
+										class="inline-block px-4 mr-1 py-1.5 font-medium text-xs leading-tight uppercase lan-button-primary"
+										data-mdb-ripple="true"
+									>
+										Preview
+									</button>
+									<button
+										type="button"
+										class="inline-block px-4 py-1.5 font-medium text-xs leading-tight uppercase lan-button-secondary"
+										data-mdb-ripple="true"
+									>
+										See demo
+									</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<!-- Item list timeline -->
+
+					<!-- <li class="flex flex-row mt-6">
 						<div class="hidden h-1 w-[480px] xl:block"></div>
 						<div class="w-6 h-[284px] xl:h-[304px]">
 							<div class="flex items-center justify-center w-6 h-6 rounded-full bg-primary">
@@ -120,7 +192,7 @@
 								</div>
 							</div>
 						</div>
-					</li>
+					</li> -->
 				</ol>
 			</div>
 			<!-- Timeline -->
@@ -130,21 +202,29 @@
 
 <script>
 import { ref } from "vue";
+
 export default {
 	name: "StoryComponent",
 	data() {
-		histories: [
-			{
-				title: "Unity Developer",
-				date: "Now",
-				desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolorum at dolor, veritatis eos cum quaerat vitae aut sit atque.",
-			},
-			{
-				title: "Kitchen Staff",
-				date: "2021-02-07",
-				desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates temporibus tempore cumque delectus dolore itaque pariatur recusandae, reprehenderit iusto voluptatem.",
-			},
-		];
+		return {
+			histories: ref([
+				{
+					title: "Unity Developer",
+					date: "Now",
+					desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolorum at dolor, veritatis eos cum quaerat vitae aut sit atque.",
+				},
+				{
+					title: "Kitchen Staff",
+					date: "2021-02-07",
+					desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates temporibus tempore cumque delectus dolore itaque pariatur recusandae, reprehenderit iusto voluptatem.",
+				},
+				{
+					title: "Tax Admin",
+					date: "2019-10-26",
+					desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates temporibus tempore cumque delectus dolore itaque pariatur recusandae, reprehenderit iusto voluptatem.",
+				},
+			]),
+		};
 	},
 	setup() {},
 };
