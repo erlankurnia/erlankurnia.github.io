44ca8ec6ca9d958abc824cff571c4c5094b90a6c
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2023-12-01 18:20:47 +0700

    Update . .

--- dist/assets/Kotlin.svg-3bc6715f.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c as s,a as t}from"./index-30e38196.js";const n={name:"KotlinIcon"},c={role:"img",width:"100%",class:"fill-current",viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg"},_=t("title",null,"Kotlin",-1),a=t("path",{d:"M24 24H0V0h24L12 12Z"},null,-1),l=[_,a];function r(i,d,p,h,f,m){return e(),s("svg",c,l)}const w=o(n,[["render",r]]);export{w as default};
