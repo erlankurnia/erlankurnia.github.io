aa518cae3e431a03ac635931ba8df934a7733528
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2024-02-19 10:42:36 +0700

    Update . .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
 	<ContactComponent class="bg-tertiary"></ContactComponent>
 </template>
 
-<script setup>
+<script>
 import HeroComponent from "../components/HeroComponent.vue";
 import IntroComponent from "../components/IntroComponent.vue";
 import ContactComponent from "../components/ContactComponent.vue";
