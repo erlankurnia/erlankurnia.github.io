d6c23e378e7d383f2b748fbdab9286821dde2281
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2024-02-19 10:18:52 +0700

    Update LoadingComponent.vue

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -1,5 +1,5 @@
 <template>
-	<section id="loading-component" class="w-full h-16 py-0">
+	<section id="loading-component" class="w-full h-64 py-0">
 		<div class="flex items-center justify-center w-full h-full p-5">
 			<div class="flex space-x-2 animate-pulse">
 				<div class="w-3 h-3 rounded-full bg-primary"></div>
