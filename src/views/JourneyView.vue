f5ec32c000e6efbde1cbda6bfaae601adf2ba85b
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2024-06-19 21:28:36 +0700

    Add journey date . .

--- src/views/JourneyView.vue
+++ src/views/JourneyView.vue
@@ -12,16 +12,20 @@
 					<p class="font-medium text-md text-secondary md:text-lg" v-if="dataUser.journey.description"
 						v-html="dataUser.journey.description"></p>
 				</div>
-				<div class="w-full max-w-4xl px-4 mx-auto sm:px-8">
-					<ul class="flex flex-col gap-6">
+				<div class="w-full max-w-4xl px-3 mx-auto sm:px-6">
+					<ul class="flex flex-col gap-8">
 						<li v-for="(data, index) in dataUser.journey.articles" :key="index"
-							class="flex flex-col w-full h-auto">
-							<RouterLink :to="'/article/' + data.id" class="mb-2 text-xl font-bold text-primary">
-								<h4 v-html="data.title"></h4>
+							class="flex flex-col w-full h-auto px-4 py-3 rounded-xl hover:bg-quaternary">
+							<RouterLink :to="'/article/' + data.id">
+								<div class="mb-2 text-xl font-bold text-primary">
+									<h4 v-html="data.title"></h4>
+								</div>
+								<p class="text-sm text-secondary" v-if="data.description"
+									v-html="('' + data.description).substring(0, data.description.length > 128 ? 128 : data.description.length)">
+								</p>
+								<p v-if="data.date" class="text-xs font-light text-right text-secondary">{{ data.date }}
+								</p>
 							</RouterLink>
-							<p class="text-sm text-secondary" v-if="data.description"
-								v-html="('' + data.description).substring(0, data.description.length > 128 ? 128 : data.description.length)">
-							</p>
 						</li>
 					</ul>
 				</div>
@@ -32,12 +36,22 @@
 </template>
 
 <script>
-import { inject } from "vue";
+import { inject, watchEffect } from "vue";
 
 export default {
 	name: "JourneyView",
 	setup() {
 		const { dataUser } = inject("dataUser");
+		watchEffect(() => {
+			if (dataUser?.journey?.articles != null && Array.isArray(dataUser.journey.articles)) {
+				dataUser.journey.articles.sort((a, b) => {
+					const dateA = new Date(a.date);
+					const dateB = new Date(b.date);
+					return dateA.getTime() - dateB.getTime();
+				});
+				dataUser.journey.articles.reverse();
+			}
+		});
 		return {
 			dataUser,
 		};
