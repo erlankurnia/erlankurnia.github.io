1de06076c83d74a22b23c84ae520c93d4ebfcce5
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2024-06-23 22:40:12 +0700

    Update skills data and UI . .

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -7,30 +7,39 @@
 				</div>
 			</div>
 			<!-- Current Ability -->
-			<div class="w-full p-4">
-				<div class="mx-auto mb-4 text-center">
-					<h2 class="lan-section-subtitle" v-html="dataUser.skill.title"></h2>
-					<p class="font-medium text-md text-secondary md:text-lg" v-html="dataUser.skill.description"></p>
-				</div>
-			</div>
-			<div class="w-full px-4">
-				<div class="flex flex-wrap items-center justify-center">
-					<!-- Skill Icon -->
-					<div
-						v-for="skill in dataUser.skill.skills"
-						class="max-w-[52px] mx-4 py-4 text-[#FFCA28] lan-50-to-100 xl:mx-6 flex flex-col"
-					>
-						<!-- Icon -->
-						<TechnologyIcon :techName="skill.name"></TechnologyIcon>
-						<!-- Icon -->
-						<h4 class="mt-3 text-xs font-bold text-center min-w-max text-secondary">
-							{{ getYearDiff(skill.year) < 1 ? "<" : "" }}{{ getYearDiff(skill.year) > 0 ? getYearDiff(skill.year) : 1 }}
-							{{ getYearDiff(skill.year) > 1 ? "years" : "year" }}
-						</h4>
+			<template v-if="dataUser.skill">
+				<div v-if="dataUser.skill.title" class="w-full p-4">
+					<div class="mx-auto mb-4 text-center">
+						<h2 class="lan-section-subtitle" v-html="dataUser.skill.title"></h2>
+						<p class="font-medium text-md text-secondary md:text-lg" v-html="dataUser.skill.description">
+						</p>
 					</div>
-					<!-- Skill Icon -->
 				</div>
-			</div>
+				<div v-if="dataUser.skill.skills && dataUser.skill.skills.length > 0" class="w-full px-4">
+					<div
+						class="grid items-center justify-center grid-cols-2 gap-2 md:grid-cols-3 md:gap-5 xl:grid-cols-4 xl:gap-8">
+						<!-- Skill Icon -->
+						<div v-for="skill in dataUser.skill.skills"
+							class="grid justify-between h-16 lg:h-20 grid-cols-[2fr_5fr] gap-3 p-2 text-secondary lan-50-to-100">
+							<!-- Icon -->
+							<div class="flex items-center justify-center w-12 h-12 lg:h-16 lg:w-16">
+								<TechnologyIcon :techName="skill.name"></TechnologyIcon>
+							</div>
+							<!-- Icon -->
+
+							<div class="flex flex-col text-xs text-left justify-evenly">
+								<h3 class="w-full text-[0.8rem] font-bold">{{ skill.title }}</h3>
+								<h4 class="w-full">
+									{{ getYearDiff(skill.year) < 1 ? "<" : "" }}{{ getYearDiff(skill.year) > 0 ?
+										getYearDiff(skill.year) : 1 }}
+										{{ getYearDiff(skill.year) > 1 ? "years" : "year" }}
+								</h4>
+							</div>
+						</div>
+						<!-- Skill Icon -->
+					</div>
+				</div>
+			</template>
 			<!-- Current Ability -->
 
 			<!-- New Ability -->
@@ -38,23 +47,30 @@
 				<div v-if="dataUser.learn.title" class="w-full p-4 mt-8">
 					<div class="mx-auto mb-4 text-center">
 						<h2 class="lan-section-subtitle" v-html="dataUser.learn.title"></h2>
-						<p class="font-medium text-md text-secondary md:text-lg" v-html="dataUser.learn.description"></p>
+						<p class="font-medium text-md text-secondary md:text-lg" v-html="dataUser.learn.description">
+						</p>
 					</div>
 				</div>
 				<div v-if="dataUser.learn.topics && dataUser.learn.topics.length > 0" class="w-full px-4">
-					<div class="flex flex-wrap items-center justify-center">
+					<div
+						class="grid items-center justify-center grid-cols-2 gap-2 md:grid-cols-3 md:gap-5 xl:grid-cols-4 xl:gap-8">
 						<!-- Skill Icon -->
-						<div
-							v-for="skill in dataUser.learn.topics"
-							class="max-w-[52px] mx-4 py-4 text-[#FFCA28] lan-50-to-100 xl:mx-6 flex flex-col"
-						>
+						<div v-for="skill in dataUser.learn.topics"
+							class="grid justify-between h-16 lg:h-20 grid-cols-[2fr_5fr] gap-3 p-2 text-secondary lan-50-to-100">
 							<!-- Icon -->
-							<TechnologyIcon :techName="skill.name"></TechnologyIcon>
+							<div class="flex items-center justify-center w-12 h-12 lg:h-16 lg:w-16">
+								<TechnologyIcon :techName="skill.name"></TechnologyIcon>
+							</div>
 							<!-- Icon -->
-							<h4 class="mt-3 text-xs font-bold text-center min-w-max text-secondary">
-								{{ getYearDiff(skill.year) < 1 ? "<" : "" }}{{ getYearDiff(skill.year) > 0 ? getYearDiff(skill.year) : 1 }}
-								{{ getYearDiff(skill.year) > 1 ? "years" : "year" }}
-							</h4>
+
+							<div class="flex flex-col text-xs text-left justify-evenly">
+								<h3 class="w-full text-[0.8rem] font-bold">{{ skill.title }}</h3>
+								<h4 class="w-full">
+									{{ getYearDiff(skill.year) < 1 ? "<" : "" }}{{ getYearDiff(skill.year) > 0 ?
+										getYearDiff(skill.year) : 1 }}
+										{{ getYearDiff(skill.year) > 1 ? "years" : "year" }}
+								</h4>
+							</div>
 						</div>
 						<!-- Skill Icon -->
 					</div>
