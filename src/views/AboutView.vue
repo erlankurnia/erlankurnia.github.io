b6e0f4ce68931a2114c3316e459affcec2f9cb0f
krlan2789 <ahlipdkno.2@gmail.com>
2025-09-10 11:30:39 +0700

    Use categorized skills component instead of previous skills component . .

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,9 +3,10 @@ import { onBeforeUnmount } from 'vue';
 import { useHead } from '@unhead/vue';
 import ProfileComponent from "@/components/ProfileComponent.vue";
 import StoryComponent from "@/components/StoryComponent.vue";
-import SkillsComponent from "@/components/SkillsComponent.vue";
+// import SkillsComponent from "@/components/SkillsComponent.vue";
 import EquipmentComponent from "@/components/EquipmentComponent.vue";
 import NextPageComponent from '@/components/NextPageComponent.vue';
+import SkillsCategorizedComponent from '@/components/SkillsCategorizedComponent.vue';
 
 const aboutHead = useHead({
 	title: "About Erlan Kurnia",
@@ -20,7 +21,8 @@ onBeforeUnmount(() => {
 	<div :class="['page-up', $attrs.class]">
 		<ProfileComponent class="bg-quaternary dark:bg-quaternaryDark"></ProfileComponent>
 		<StoryComponent class="bg-tertiary dark:bg-tertiaryDark"></StoryComponent>
-		<SkillsComponent class="bg-quaternary dark:bg-quaternaryDark"></SkillsComponent>
+		<!-- <SkillsComponent class="bg-quaternary dark:bg-quaternaryDark"></SkillsComponent> -->
+		<SkillsCategorizedComponent class="bg-quaternary dark:bg-quaternaryDark"></SkillsCategorizedComponent>
 		<EquipmentComponent class="bg-tertiary dark:bg-tertiaryDark"></EquipmentComponent>
 		<NextPageComponent path="/notes" label="Goto Notes"></NextPageComponent>
 	</div>
