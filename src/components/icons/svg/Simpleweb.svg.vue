3662dad3747ab9fc287b711a2dfc34ed400c2630
krlan2789 <ahlipdkno.2@gmail.com>
2026-02-18 16:08:23 +0700

    Add some icons . .

--- src/components/icons/svg/Simpleweb.svg.vue
+++ src/components/icons/svg/Simpleweb.svg.vue
@@ -1,9 +1,19 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" width="100%" class="fill-current" viewBox="0 0 48 48">
+    <svg width="100%" viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" fill="none">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier">
+            <circle cx="96" cy="96" r="74" stroke="currentColor" stroke-width="12"></circle>
+            <ellipse cx="96" cy="96" stroke="currentColor" stroke-width="12" rx="30" ry="74"></ellipse>
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"
+                d="M28 72h136M28 120h136"></path>
+        </g>
+    </svg>
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" width="100%" class="fill-current" viewBox="0 0 48 48">
         <g fill="none" stroke="currentColor" stroke-width="3">
             <path stroke-linejoin="round" d="M3 24a21 21 0 1 0 42 0a21 21 0 1 0-42 0" />
             <path stroke-linejoin="round" d="M15 24a9 21 0 1 1 18 0a9 21 0 1 1-18 0" />
             <path stroke-linecap="round" d="M4.5 31h39m-39-14h39" />
         </g>
-    </svg>
+    </svg> -->
 </template>
