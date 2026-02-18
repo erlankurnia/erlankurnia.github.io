adc6723fa738ec4de1de4bc965468e47e192b84e
krlan2789 <ahlipdkno.2@gmail.com>
2026-02-18 16:21:00 +0700

    Update EquipmentComponent.vue

--- src/components/EquipmentComponent.vue
+++ src/components/EquipmentComponent.vue
@@ -30,14 +30,14 @@ watch(() => dataEndpoint?.value, async (newVal) => {
 		const resEquipment = (await tools.fetch<IEquipment>(newVal.equipment));
 		dataEquipment.value = resEquipment.data ?? dataEquipment.value;
 
-		const resHardware = (await tools.fetch<Record<string, IEquipmentItem[]>>(newVal.hardware));
-		if (dataEquipment.value.hardware && resHardware.data) {
-			dataEquipment.value.hardware.equipments = resHardware.data;
-		}
-
 		const resSoftware = (await tools.fetch<Record<string, IEquipmentItem[]>>(newVal.software));
 		if (dataEquipment.value.software && resSoftware.data) {
 			dataEquipment.value.software.equipments = resSoftware.data;
+		}
+
+		const resHardware = (await tools.fetch<Record<string, IEquipmentItem[]>>(newVal.hardware));
+		if (dataEquipment.value.hardware && resHardware.data) {
+			dataEquipment.value.hardware.equipments = resHardware.data;
 		}
 	}
 }, { immediate: true });
@@ -50,103 +50,59 @@ watch(() => dataEndpoint?.value, async (newVal) => {
 				<h2 class="mb-3 lan-section-title">EQUIPMENTS</h2>
 			</div>
 
-			<!-- Hardware -->
-			<div class="flex flex-wrap" v-if="dataEquipment.hardware && props.showHardware">
-				<div class="w-full text-center">
-					<h3 class="lan-section-subtitle" v-html="dataEquipment.hardware.title"></h3>
-					<p class="lan-section-desc" v-html="dataEquipment.hardware.description"></p>
-				</div>
-				<!-- <div v-if="dataEquipment.hardware.image" class="w-full px-4 lg:w-1/3">
-					<div class="relative my-10 lg:my-9 lg:right-0">
-						<img :src="tools.getCdnUrl(dataEquipment.hardware.image)"
-							class="w-full mx-auto max-md:rounded-xl lg:rounded-tr-xl lg:rounded-bl-xl" />
+			<!-- Equipments -->
+			<template v-for="(equipment, index) in dataEquipment" :key="index">
+				<div class="flex flex-wrap my-8" v-if="equipment && ((index == 'software' && props.showSoftware) || (index == 'hardware' && props.showHardware))">
+					<div class="w-full text-center">
+						<h3 class="lan-section-subtitle" v-html="equipment.title"></h3>
+						<p class="lan-section-desc" v-html="equipment.description"></p>
 					</div>
-				</div> -->
-				<div class="w-full px-4 mt-6">
-					<table v-if="dataEquipment.hardware && dataEquipment.hardware.equipments"
-						class="w-full mb-6 text-sm font-medium border-separate table-auto text-secondary dark:text-secondaryDark sm:text-base">
-						<!--  lg:ml-4 2xl:ml-8 4xl:ml-12 -->
-						<thead>
-							<tr>
-								<th></th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-							<template v-for="(item, key) in dataEquipment.hardware.equipments" :key="key">
-								<tr
-									class="w-auto grid grid-cols-[150px_auto_1fr] md:grid-cols-[168px_auto_1fr] max-lg:py-2">
-									<td class="flex flex-row pt-5 lg:pt-7 align-top text-primary dark:text-primaryDark">
-										<div class="max-h-[24px] lg:max-h-[28px] -mt-0.5 mr-1.5 aspect-square lg:mr-3">
-											<Icon :techName="key"></Icon>
-										</div>
-										<h3 class="" v-html="key"></h3>
-									</td>
-									<td class="w-1 md:w-2 xl:w-4 2xl:w-6"></td>
-									<td class="grid grid-cols-1 xl:grid-cols-2 3xl:grid-cols-3 pt-4 align-text-top">
-										<template v-for="data in item">
-											<button type="button" @click="showOptions($event, data)"
-												class="max-h-[36px] lg:max-h-[44px] w-fit max-w-xs py-2 lan-50-to-100 flex flex-row">
-												<div class="h-full mr-2 aspect-square lg:mr-4">
-													<Icon :techName="'' + data.icon"></Icon>
-												</div>
-												<h4 class="h-auto my-auto text-xs text-left font-bold min-w-min text-secondary dark:text-secondaryDark"
-													v-html="data.name"></h4>
-											</button>
-										</template>
-									</td>
+					<!-- <div v-if="dataEquipment.hardware.image" class="w-full px-4 lg:w-1/3">
+						<div class="relative my-10 lg:my-9 lg:right-0">
+							<img :src="tools.getCdnUrl(dataEquipment.hardware.image)"
+								class="w-full mx-auto max-md:rounded-xl lg:rounded-tr-xl lg:rounded-bl-xl" />
+						</div>
+					</div> -->
+					<div class="w-full px-4">
+						<table v-if="equipment && equipment.equipments"
+							class="w-full mb-6 text-sm font-medium border-separate table-auto text-secondary dark:text-secondaryDark sm:text-base">
+							<thead>
+								<tr>
+									<th></th>
+									<th></th>
 								</tr>
-							</template>
-						</tbody>
-					</table>
+							</thead>
+							<tbody>
+								<template v-for="(item, key) in equipment.equipments" :key="key">
+									<tr
+										class="w-auto grid grid-cols-[150px_auto_1fr] md:grid-cols-[168px_auto_1fr] max-lg:py-2">
+										<td class="flex flex-row pt-6 lg:pt-7 align-top text-primary dark:text-primaryDark">
+											<div class="max-h-[24px] lg:max-h-[28px] -mt-0.5 mr-1.5 aspect-square lg:mr-3">
+												<Icon :techName="key"></Icon>
+											</div>
+											<h3 class="" v-html="key"></h3>
+										</td>
+										<td class="w-1 md:w-2 xl:w-4 2xl:w-6"></td>
+										<td class="grid grid-cols-1 xl:grid-cols-2 3xl:grid-cols-3 pt-4 align-text-top">
+											<template v-for="data in item">
+												<button type="button" @click="showOptions($event, data)"
+													class="max-h-[36px] lg:max-h-[44px] w-fit max-w-xs py-2 lan-50-to-100 flex flex-row">
+													<div class="h-full mr-2 aspect-square lg:mr-4">
+														<Icon :techName="'' + data.icon"></Icon>
+													</div>
+													<h4 class="h-auto my-auto text-xs text-left font-bold min-w-min line-clamp-2 text-secondary dark:text-secondaryDark"
+														v-html="data.name"></h4>
+												</button>
+											</template>
+										</td>
+									</tr>
+								</template>
+							</tbody>
+						</table>
+					</div>
 				</div>
-			</div>
-			<!-- Hardware -->
-
-			<div class="w-full h-16"></div>
-
-			<!-- Software -->
-			<div class="flex flex-wrap" v-if="dataEquipment.software && props.showSoftware">
-				<div class="w-full text-center">
-					<h3 class="lan-section-subtitle" v-html="dataEquipment.software.title"></h3>
-					<p class="lan-section-desc" v-html="dataEquipment.software.description"></p>
-				</div>
-				<div class="w-full px-4">
-					<table v-if="dataEquipment.software && dataEquipment.software.equipments"
-						class="w-full mb-6 text-sm font-medium border-separate table-auto text-secondary dark:text-secondaryDark sm:text-base">
-						<thead>
-							<tr>
-								<th></th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-							<template v-for="(item, key) in dataEquipment.software.equipments" :key="key">
-								<tr
-									class="w-auto grid grid-cols-[150px_auto_1fr] md:grid-cols-[168px_auto_1fr] max-lg:py-2">
-									<td class="pt-5 lg:pt-7 align-top text-primary dark:text-primaryDark">
-										<h3 class="" v-html="key"></h3>
-									</td>
-									<td class="w-3 md:w-4 xl:w-7 2xl:w-10"></td>
-									<td class="grid grid-cols-1 xl:grid-cols-2 3xl:grid-cols-3 pt-4 align-text-top">
-										<template v-for="data in item">
-											<button type="button" @click="showOptions($event, data)"
-												class="max-h-[36px] lg:max-h-[44px] w-fit max-w-xs py-2 lan-50-to-100 flex flex-row">
-												<div class="h-full mr-2 aspect-square lg:mr-4">
-													<Icon :techName="'' + data.icon"></Icon>
-												</div>
-												<h4 class="h-auto my-auto text-xs text-left font-bold min-w-min text-secondary dark:text-secondaryDark"
-													v-html="data.name"></h4>
-											</button>
-										</template>
-									</td>
-								</tr>
-							</template>
-						</tbody>
-					</table>
-				</div>
-			</div>
-			<!-- Software -->
+			</template>
+			<!-- Equipments -->
 		</div>
 
 		<!-- <ContextMenuComponent v-if="selectedItem" ref="optionComponent">
