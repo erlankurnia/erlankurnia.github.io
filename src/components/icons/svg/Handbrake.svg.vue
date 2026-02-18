045bab8e37b78fc2122dbd444238631b9bbd710e
krlan2789 <ahlipdkno.2@gmail.com>
2026-02-18 12:30:34 +0700

    Update Handbrake.svg.vue

--- src/components/icons/svg/Handbrake.svg.vue
+++ src/components/icons/svg/Handbrake.svg.vue
@@ -1,3 +1,3 @@
 <template>
-    <img src="https://github.com/HandBrake/HandBrake/blob/5669b5e1bf0e279f43f47446789e193d071e027c/graphics/Logo/HandBrake-v1-128x128.png" alt="Logo Handbrake" width="100%">
+    <img src="https://raw.githubusercontent.com/HandBrake/HandBrake/5669b5e1bf0e279f43f47446789e193d071e027c/graphics/Logo/HandBrake-v1-128x128.png" alt="Logo Handbrake" width="100%">
 </template>
