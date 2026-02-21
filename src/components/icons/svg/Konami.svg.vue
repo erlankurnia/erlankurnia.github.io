3630ab8b5fb5d87f8533728efd8d66fdf9434948
krlan2789 <ahlipdkno.2@gmail.com>
2026-02-22 04:41:44 +0700

    Add some icons . .

---
+++ src/components/icons/svg/Konami.svg.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full aspect-square flex justify-center">
+        <img src="https://cdn.brandfetch.io/idG6XJzg4J/theme/dark/logo.svg?c=1bxid64Mup7aczewSAYMX&t=1766446318020" alt="Logo Konami" class="w-full my-auto h-auto" >
+    </div>
+</template>
